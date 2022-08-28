262d1dd0a47751b84b302a3a01d244e26ac79505
oozaer <olivier@ozaer.fr>
2022-08-28 17:31:47 +0200

    Masquage du mdp dans la config

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -33,7 +33,7 @@ if (!isConnect()) {
         <div class="form-group">
             <label class="col-lg-4 control-label">{{mot de passe}}</label>
             <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="motdepasse" />
+                <input class="configKey form-control" data-l1key="motdepasse" type="password"/>
             </div>
         </div>
   </fieldset>
