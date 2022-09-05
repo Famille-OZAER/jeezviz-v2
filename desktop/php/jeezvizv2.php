f6a201b5b9806b3ed94cb13c41e8bbae6232e5b5
oozaer <olivier@ozaer.fr>
2022-09-05 21:19:35 +0200

    Ajout cases Historiser / Afficher sur commandes

--- desktop/php/jeezvizv2.php
+++ desktop/php/jeezvizv2.php
@@ -110,7 +110,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
         <table id="table_cmd" class="table table-bordered table-condensed">
             <thead>
                 <tr>
-                <th>{{ID}}</th><th>{{Nom}}</th><th>{{Type}}</th><th>{{Action}}</th>
+                <th>{{ID}}</th><th>{{Nom}}</th><th>{{Type}}</th><th>{{Paramètres}}</th><th>{{Action}}</th>
                 </tr>
             </thead>
             <tbody>
